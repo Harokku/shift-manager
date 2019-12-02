9de23484e27b067b3f5f6e93ef0b79aa3f099d01
harokku <crenna.simone@gmail.com>
2019-12-02 16:55:08 +0100

    Initial commit with Echo setted up and basic GSheet service

---
+++ go.mod
@@ -0,0 +1,12 @@
+module shift-manager
+
+go 1.13
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/lib/pq v1.2.0
+	golang.org/x/crypto v0.0.0-20191202143827-86a70503ff7e // indirect
+	google.golang.org/api v0.14.0
+)
---
+++ server.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	_ "github.com/lib/pq"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	// Heroku port from env variable
+	port := os.Getenv("PORT")
+	fmt.Printf("port set to %v\n", port)
+
+	// -----------------------
+	// Database connection config
+	// -----------------------
+
+	// Heroku Postgres connection and ping
+	dbConn, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	checkErrorAndPanic(err)
+
+	defer dbConn.Close()
+
+	err = dbConn.Ping()
+	checkErrorAndPanic(err)
+	fmt.Println("Correctly pinged DB")
+
+	// -----------------------
+	// Echo server definition
+	// -----------------------
+
+	e := echo.New()
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "time=${time_rfc3339}, method=${method}, uri=${uri}, status=${status}, latency=${latency_human}\n",
+	}))
+
+	// -----------------------
+	// Static definition
+	// -----------------------
+	e.File("/favicon.ico", "static/favicon.ico")
+
+	e.GET("/ping", func(context echo.Context) error {
+		return context.JSON(http.StatusOK, "Pong")
+	})
+
+	// -----------------------
+	// Server Start
+	// -----------------------
+
+	e.Logger.Fatal(e.Start(":" + port))
+}
+
+// Default error check with fatal if err != nil
+func checkErrorAndPanic(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
