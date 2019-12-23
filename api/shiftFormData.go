5b8ec775565584c4f0c06b7074f68ec1f570fff2
Crenna Simone <Harokku@users.noreply.github.com>
2019-12-23 19:32:15 +0100

    Quick fixup for returned worked shift form selects (#9)
    * Initial commit with Echo setted up and basic GSheet service
    
    * Added .gitignore file
    
    * FEAT: Implemented base crypto with hash and password check in separateda pkg
    
    * FEAT: Implemented get user Dbcall with test
    
    * FEAT: Implemented Login func with JWD signing with claims
    
    * FEAT: Implemented user db calls
    
    Get single user with hashed pwd
    Create new user
    Reset user pws
    Delete user
    
    REFACTOR: Moved login.go in db package for better suite and to avoid circular import
    
    * FEAT: Implemented partial admin route group with password reset endpoint
    
    * FIX: Added some entry in .gitignore and fixed routes
    
    * G sheet (#3)
    
    * FEAT: Initial Gsheet append method with basic marshalling
    
    * FEAT: Implemented marshal to gsuite type method
    
    TODO: Write tests
    
    * FEAT: Added recoverer and CORS
    
    * FIX: Fixed bug with reset password
    
    Now return corrected username after successful reset
    
    * FEAT: Implemented methods to retrieve associated user's detail
    
    Added protected GET endpoint to retrieve user's detail based on username claims from JWT
    
    * FIX: Added location to sheets.go Type to be in line with requirement
    
    * FEAT: added db func to get shift form data and GET endpoint that return a single object containing all the data
    
    * FIX: Removed test routes
    
    * FIX: Changed form data struct Vehicle to Vehicles as expected from client and to be il line with others fields

--- api/shiftFormData.go
+++ api/shiftFormData.go
@@ -12,7 +12,7 @@ func GetAllFormData(s *db.Service) echo.HandlerFunc {
 		var response = struct {
 			Locations []db.Location     `json:"locations,omitempty"`
 			Shifts    []db.Shift        `json:"shifts,omitempty"`
-			Vehicle   []db.Vehicle      `json:"vehicle,omitempty"`
+			Vehicles  []db.Vehicle      `json:"vehicles,omitempty"`
 			Roles     []db.OperatorRole `json:"roles,omitempty"`
 		}{}
 
@@ -52,7 +52,7 @@ func GetAllFormData(s *db.Service) echo.HandlerFunc {
 			return context.String(http.StatusBadRequest, fmt.Sprintf("Error retrieving vehicles: %v\n", err))
 		}
 
-		response.Vehicle = vehicles
+		response.Vehicles = vehicles
 
 		// Roles retrieval
 		var roles []db.OperatorRole
